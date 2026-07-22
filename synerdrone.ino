5efd55f52ade66ac72ac47e99835253c3e5168ae
Lorenzo Bivens <git@lorenzobivens.info>
2026-07-22 10:44:56 -0700

    removing wip wifi feature

--- synerdrone.ino
+++ synerdrone.ino
@@ -4,25 +4,14 @@
   Platform ESP32 Arduino >= 3.3.0
 */
 
-#include <WiFi.h>
 #include "USB.h"
 #include "USBHIDMouse.h"
-
-char ssid[] = "yourNetwork";
-char pass[] = "secretPassword";
-int status = WL_IDLE_STATUS;
 
 USBHIDMouse Mouse;
 long x, y;
 bool running = false;
 
 void setup() {
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(ssid);
-    status = WiFi.begin(ssid, pass);
-    delay(10000);
-  }
 
   // This is the only button we have 
   pinMode(BOOT_PIN, INPUT_PULLUP);
